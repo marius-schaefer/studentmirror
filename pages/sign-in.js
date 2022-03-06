a61b16ff53878a62d3d55ed8f646e72d99146dbe
marius-schaefer <marius.schaefer2005@gmail.com>
2022-03-06 19:30:02 +0200

    Add sign-in.js

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -3,11 +3,12 @@ import styles from '../styles/Home.module.css'
 
 import { supabase } from "../client";
 
-export default function SignIn {
+export default function SignIn() {
     const [email, setEmail] = useState('')
     const [submitted, setSubmitted] = useState(false)
 
     async function signIn() {
+        if (!email) return // ADD FORM VALIDATION HERE
         const {error, data} = await supabase.auth.signIn({
             email
         })
@@ -17,4 +18,27 @@ export default function SignIn {
             setSubmitted(true )
         }
     }
+    //Submitted UI State:
+    if (submitted) {
+        return (
+            <div>
+                <h1>Please check your email to sign in</h1> 
+            </div>
+        )//UPDATE THE POST FORM UI HERE ^^^
+    }
+    // Default UI State:
+    return (
+        <div>
+            <main>
+                <h1>
+                    Sign In
+                </h1>
+                <input
+                    onChange={e => setEmail(e.target.value)}
+                    style={{margin: 10}}
+                />
+                <button onClick={() => signIn()}>Sign In</button>
+            </main>
+        </div>
+    )
 }
