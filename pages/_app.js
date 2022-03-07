f90df15d78c607252f4c024ed09ad74658f84850
marius-schaefer <marius.schaefer2005@gmail.com>
2022-03-07 12:29:46 +0200

    Add Nav + Authentication Cookie

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,61 @@
 import '../styles/index.css'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { supabase } from '../client'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [authenticatedState, setAuthenticatedState] = useState('not-authenticated')
+  const router = useRouter()
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      handleAuthChange(event, session)
+      if (event === 'SIGNED_IN') {
+        setAuthenticatedState('authenticated')
+        router.push('/')
+      }
+      if (event === 'SIGNED_OUT') {
+        setAuthenticatedState('not-authenticated')
+      }
+    })
+    checkUser()
+    return () => {
+      authListener.unsubscribe()
+    }
+  }, [])
+  
+  async function checkUser() {
+    const user = await supabase.auth.user()
+    if (user) {
+      setAuthenticatedState('authenticated')
+    }
+  }
+
+  async function handleAuthChange(event, session) {
+    await fetch('/api/auth', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      credentials: 'same-origin',
+      body: JSON.stringify({ event, session }),
+    })
+  }
+
+  return (
+    <div>
+      <nav>
+        <Link href='/'>
+          <a>Home</a>
+        </Link>
+        {
+          authenticatedState === 'not-authenticated' && (
+            <Link href='/sign-in'>
+              <a>Sign In</a>
+            </Link>
+          )
+        }
+      </nav>
+      <Component {...pageProps} />
+    </div>)
 }
 
 export default MyApp
