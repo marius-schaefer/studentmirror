b4db5c08483a08a743aa10cae075a149f47c17b6
marius-schaefer <marius.schaefer2005@gmail.com>
2022-03-07 12:28:59 +0200

    Update Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import styles from '../styles/Home.module.css'
+import { supabase } from '../client'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    fetchLogInStatus()
+  }, [])
+
+  async function fetchLogInStatus() {
+    const logInStatus = await supabase.auth.user()
+    if (!logInStatus) {
+      router.push('/sign-in')
+    }
+    return
+  }
+
   return (
     <div>
       <main>
-        <h1 className="text-3xl font-bold">Student Mirror</h1>
-        <h2 className="text-3xl">Hello World</h2>
+        <h1 className="text-3xl font-bold">Test Succesful</h1>
+        <h2 className="text-3xl">Go do work dipshit</h2>
       </main>
     </div>
   )
